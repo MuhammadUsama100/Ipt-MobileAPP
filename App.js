b2412f11d33135329b516fbccfc4203c4f0189e1
Mohammad Areeb Siddiqui <mohammadareebs@gmail.com>
2021-12-18 14:27:13 +0500

    Basic architecture

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import * as React from 'react';
-import {AppRegistry, View} from 'react-native';
-import {Provider as PaperProvider, Text} from 'react-native-paper';
-import {NavigationContainer} from '@react-navigation/native';
+import {AppRegistry} from 'react-native';
+import {
+  Provider as PaperProvider, 
+  DefaultTheme as PaperDefaultTheme,
+} from 'react-native-paper';
+import {
+  NavigationContainer,
+} from '@react-navigation/native';
 import {name as appName} from './app.json';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SCREEN_NAMES} from './constants/screens.constants';
-import LoginScreen from './src/screens/LoginScreen';
-import SplashScreen from './src/screens/SplashScreen';
-import SignupScreen from './src/screens/SignupScreen';
+import LoginScreen from './src/components/LoginScreen';
+import SplashScreen from './src/components/SplashScreen';
+import SignupScreen from './src/components/SignupScreen';
+import NavigationBar from './src/shared/NavigationBar';
+import HomeNavigator from './src/components/HomeNavigator';
 
 const Stack = createNativeStackNavigator();
 
+const theme = {
+  ...PaperDefaultTheme,
+  colors: {
+    ...PaperDefaultTheme.colors,
+    background: '#f2eee3',
+    primary: '#aaa',
+  },
+};
+
 export default function Main() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName={SCREEN_NAMES.SPLASH}>
+        <Stack.Navigator 
+          initialRouteName={SCREEN_NAMES.HOME_NAVIGATOR}
+          screenOptions={{
+            header: NavigationBar,
+          }}
+        >
           <Stack.Screen
             name={SCREEN_NAMES.SPLASH}
             component={SplashScreen}
@@ -33,6 +54,13 @@ export default function Main() {
           <Stack.Screen
             name={SCREEN_NAMES.SIGNUP}
             component={SignupScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name={SCREEN_NAMES.HOME_NAVIGATOR}
+            component={HomeNavigator}
             options={{
               headerShown: false,
             }}
--- constants/screens.constants.js
+++ constants/screens.constants.js
@@ -2,4 +2,10 @@ export const SCREEN_NAMES = {
   SPLASH: 'Splash',
   LOGIN: 'Login',
   SIGNUP: 'Sign Up',
+  HOME_NAVIGATOR: "HomeNavigator",
+  HOME: 'Home',
+  REGISTER_COMPLAINT: 'Register Complaint',
+  BROWSE_COMPLAINTS: 'Browse Complaints',
+  MY_COMPLAINTS: 'My Complaints',
+
 };
